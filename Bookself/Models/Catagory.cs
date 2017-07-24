ca0ff3d3e58e4043a60f78bf7323a5bb13b71f0e
ShawnBorn-Newton <shawn.newton3@gmail.com>
2017-07-24 14:17:56 -0400

    added book and catagory models

---
+++ Bookself/Models/Catagory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Bookself.Models
+{
+    public class Catagory
+    {
+        [Key]
+        public int ID { get; set; }
+        public string name { get; set; }
+        public virtual ICollection<book> Books { get; set; }
+    }
+}
---
+++ Bookself/Models/book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+
+namespace Bookself.Models
+{
+    public class book
+    {
+        [Key]//Primary Key
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public DateTime PublishedDate{ get; set; }
+        public string Description { get; set; }
+        public string ISBN { get; set; }
+        [ForeignKey("Catagory")]
+        public int CatagoryID { get; set; }
+        public virtual Catagory Catagory { get; set; }// Navigation Property
+    }
+
+}
